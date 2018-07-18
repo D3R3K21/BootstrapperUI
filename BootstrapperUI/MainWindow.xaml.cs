b7af322c736b0d375b238e4e05de980fefda6c55
Derek Rose <derek.s.rose21@gmail.com>
2018-07-18 10:22:08 -0700

    setting env variable

--- BootstrapperUI/MainWindow.xaml.cs
+++ BootstrapperUI/MainWindow.xaml.cs
@@ -94,7 +94,16 @@ namespace BootstrapperUI
                         entries.ForEach(p => writer.WriteLine(p + "\n"));
                     }
                 }
-
+                string setting = null;
+                try
+                {
+                    setting = $"{new Uri(Endpoint).DnsSafeHost}:8500/v1/kv/win";
+                    Environment.SetEnvironmentVariable("CONSUL_SERVER", setting, EnvironmentVariableTarget.Machine);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Failed to set 'CONSUL_SERVER' environment variable to '{setting}'");
+                }
 
             }
             catch (Exception ex)
